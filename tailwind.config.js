554cd2b87186a0005e49e33b5c43595832cf09ae
Abdullah <muhammadabdullahbaig3750@gmail.com>
2025-08-09 00:37:39 +0500

    Tailwind css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [ './*.html',
-    './public/**/*.html',
-    './src/**/*.js'],
+  content: [
+    './public/*.html',        // HTML files directly in public folder
+    './public/**/*.html',     // HTML files in public subfolders  
+    './public/js/**/*.js',    // JS files in public/js folder
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
 }
-
